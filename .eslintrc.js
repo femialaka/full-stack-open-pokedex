141dbedd122f1c9d645ce708bcb66ee887700032
Femi Alaka <femialakadev@gmail.com>
2024-02-10 01:28:31 +0000

    cypress update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "globals": {
     "document": "readonly"
@@ -19,7 +20,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
